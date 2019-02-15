0c67aed3fb785d5fde1faa40783f0216dbef56a9
Peilong Li <straydragonl@foxmail.com>
2019-02-15 22:50:39 +0800

    modify to skip some blog-related tests

--- tests/test_cmd_blog.py
+++ tests/test_cmd_blog.py
@@ -1,21 +1,22 @@
-# Warning: I don't know how to test real commands now...
+import pytest
+from click.testing import CliRunner
 
-# import pytest
-# from click.testing import CliRunner
-#
-#
-# @pytest.fixture
-# def blog_fixture():
-#     pass
-#
 
-# def test_blog_publish():
-#     from commands.cmd_blog import publish, Blog
-#     runner = CliRunner()
-#     after_call = runner.invoke(publish)
-#     result = after_call.exit_code
-#     expected = 0
-#     assert expected == result
+@pytest.mark.skip(reason="At present, I have not found a suitable way to test real commands...")
+def test_publish():
+    from commands.cmd_blog import publish
+    runner = CliRunner()
+    after_call = runner.invoke(publish)
+    result = after_call.exit_code
+    expected = 0
+    assert expected == result
 
-# def test_blog_update():
-#     assert Blog.update() is False
+
+@pytest.mark.skip(reason="At present, I have not found a suitable way to test real commands...")
+def test_finish():
+    from commands.cmd_blog import finish
+    runner = CliRunner()
+    after_call = runner.invoke(finish)
+    result = after_call.exit_code
+    expected = 0
+    assert expected == result
