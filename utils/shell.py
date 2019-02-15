c7fb72a349189ac7f302ac65c5dc0490429cf342
Peilong Li <straydragonl@foxmail.com>
2019-02-15 17:05:59 +0800

    update and add type annotations

--- utils/shell.py
+++ utils/shell.py
@@ -1,6 +1,11 @@
 import shlex
 import subprocess
+from typing import List
 
 
-def run(command):
+def run(command: str) -> subprocess.CompletedProcess:
     return subprocess.run(shlex.split(command))
+
+
+def to_args(command: str) -> List[str]:
+    return shlex.split(command)
