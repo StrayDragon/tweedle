9c8f857e85c6f95c36be9dc73c01246b8aea67d2
Peilong Li <straydragonl@foxmail.com>
2019-02-15 22:49:51 +0800

    add project-related log and update corresponding tests

--- commands/cmd_project.py
+++ commands/cmd_project.py
@@ -1,5 +1,8 @@
 import click
 
+from utils.terminal import log
+from utils.shell import current_running_path
+
 
 @click.group('project', short_help='Generate a project structure out of the box')
 def cli():
@@ -9,11 +12,18 @@ def cli():
 
 @cli.command()
 @click.argument('project_name')
-@click.option('-L', '--lang', type=click.Choice(['cpp']))
+@click.option('-L', '--lang', type=click.Choice(['cpp']), required=True)
 @click.option('-B', '--build-tool', type=click.Choice(['cmake']))
 @click.option('-T', '--third-party', type=click.Choice(['googletest']))
 def new(project_name, lang, build_tool, third_party):
     """Generate a target project"""
-    click.echo(
-        f'Generated the "{project_name}", {lang}, {build_tool}, {third_party}')
-    pass
+
+    # Display these after success
+    log('Generated the project in ', font_color='green', bold=True, nl=False)
+    log(current_running_path())
+    log(f'  Project Name : {project_name}')
+    log(f'  Language     : {lang}')
+    if build_tool:
+        log(f'  Build Tool   : {build_tool}')
+    if third_party:
+        log(f'  External-Libs: {third_party}')
--- tests/test_cmd_project.py
+++ tests/test_cmd_project.py
@@ -1,2 +1,32 @@
-# def test_passing():
-#     assert True
+import pytest
+from click.testing import CliRunner
+
+from utils import shell
+
+
+@pytest.fixture()
+def project_new_runner():
+    print('setup')
+    runner = CliRunner()
+    yield runner
+    print('\nteardown')
+
+
+def test_passing_only_name(project_new_runner):
+    """ignore other options"""
+    from commands.cmd_project import new
+    from utils.testing import ClickDefaultErrorOutput as Error
+
+    with project_new_runner.isolated_filesystem():
+        result = project_new_runner.invoke(new, shell.to_args('test_projected'))
+
+    assert Error.MISS_OPT in result.stdout
+
+
+def test_passing_name_and_suited_opt_lang(project_new_runner):
+    from commands.cmd_project import new
+
+    with project_new_runner.isolated_filesystem():
+        result = project_new_runner.invoke(new, shell.to_args('test_projected --lang cpp'))
+
+    assert 'Generated the project' in result.stdout
