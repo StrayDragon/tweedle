f028eb0acc7a353b0e2c610d4bb99d694b1dd92e
Peilong Li <straydragonl@foxmail.com>
2019-04-18 22:56:32 +0800

    add @check_command to support external commands' requirement

--- utils/shell.py
+++ utils/shell.py
@@ -1,6 +1,7 @@
+import os
 import shlex
 import subprocess
-import os
+from functools import wraps
 from typing import List
 
 
@@ -14,3 +15,18 @@ def to_args(command: str) -> List[str]:
 
 def current_running_path() -> str:
     return os.getcwd()
+
+
+def check_command(command: str):
+    def decorate(f):
+        result = subprocess.getstatusoutput(command)
+
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            if 'not found' in result[1]:
+                return f(*args, **kwargs)
+            raise Exception(f"Error: '{command}' is not exist!")
+
+        return wrapper
+
+    return decorate
