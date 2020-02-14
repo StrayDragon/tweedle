b18d40562e9b40d0bdd5fc71b4770e3e190287d3
StrayDragon <straydragonv@gmail.com>
2020-02-14 15:18:54 +0800

    chore: dep updates

--- setup.py
+++ setup.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = \
-['dragon', 'dragon.cmd']
+packages = ['dragon', 'dragon.cmd']
 
-package_data = \
-{'': ['*']}
+package_data = {'': ['*']}
 
-install_requires = \
-['click>=7.0,<8.0', 'munch>=2.5.0,<3.0.0', 'toml>=0.10.0,<0.11.0']
+install_requires = [
+    'click>=7.0,<8.0', 'munch>=2.5.0,<3.0.0', 'toml>=0.10.0,<0.11.0'
+]
 
-entry_points = \
-{'console_scripts': ['dragon = dragon.entry:main']}
+entry_points = {'console_scripts': ['dragon = dragon.entry:main']}
 
 setup_kwargs = {
     'name': 'dragon',
@@ -29,6 +27,5 @@ setup_kwargs = {
     'entry_points': entry_points,
     'python_requires': '>=3.8,<4.0',
 }
-
 
 setup(**setup_kwargs)
